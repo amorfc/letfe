41e7e8015df49ce0938748a595c706e6df1dc62b
amorf <fatihermetin@gmail.com>
2023-09-08 23:58:10 +0300

    Add app module for getit

--- lib/infra/app_module/app_module.dart
+++ lib/infra/app_module/app_module.dart
@@ -1,8 +1,13 @@
 import 'package:get_it/get_it.dart';
+import 'package:letfe/data/data_source/remote/user/remote_user_data_source.dart';
 import 'package:letfe/infra/config/app_config.dart';
 
+/// Service locator instance
 final locator = GetIt.I;
 
+///Service locator configuration scope
 void configureDependencies() {
-  locator.registerSingleton<AppConfig>(AppConfig());
+  locator
+    ..registerSingleton<AppConfig>(AppConfig())
+    ..registerSingleton<UserDataSource>(UserDataSource());
 }
